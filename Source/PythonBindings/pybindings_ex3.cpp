588f717c8234e9044cf665022822a94e035bcf54
xenakios <xenakios@gmail.com>
2025-09-23 01:42:59 +0300

    lib version update check for client

--- Source/PythonBindings/pybindings_ex3.cpp
+++ Source/PythonBindings/pybindings_ex3.cpp
@@ -21,6 +21,11 @@ class MTSESP_Client
         m_client = MTS_RegisterClient();
         if (!m_client)
             throw std::runtime_error("Could not register MTS-ESP client");
+        if (MTS_ShouldUpdateLibraryClient(m_client))
+        {
+            throw std::runtime_error(
+                "MTS-ESP system library is out of date, you should update it!\n");
+        }
         m_enabled = enable;
     }
     ~MTSESP_Client()
