8c2d6ea50ffafd1a37f54a1eda030918391148d8
xenakios <xenakios@gmail.com>
2025-10-10 02:49:40 +0300

    tonegen tweaks

--- Source/PythonBindings/pybindings_ex4.cpp
+++ Source/PythonBindings/pybindings_ex4.cpp
@@ -10,6 +10,8 @@ namespace py = pybind11;
 inline py::array_t<float> generate_tone(xenakios::Envelope &pitch_env,
                                         xenakios::Envelope &volume_env, double sr, double duration)
 {
+    pitch_env.sortPoints();
+    volume_env.sortPoints();
     int chans = 1;
     int frames = duration * sr;
     py::buffer_info binfo(
@@ -28,17 +30,22 @@ inline py::array_t<float> generate_tone(xenakios::Envelope &pitch_env,
     int framecount = 0;
     float *writebuf = output_audio.mutable_data(0);
     volume_env.clearOutputBlock();
-    while (framecount < frames - blocksize)
+    while (framecount < frames)
     {
+        size_t framestoprocess = std::min<int>(blocksize, frames - framecount);
         double tpos = framecount / sr;
         double pitch = pitch_env.getValueAtPosition(tpos);
+        pitch = std::clamp(pitch, 0.0, 136.0);
         double hz = 440.0 * std::pow(2.0, 1.0 / 12 * (pitch - 69.0));
         osc_sin.setFrequency(hz);
         volume_env.processBlock(tpos, sr, 0, 64);
-        for (int i = 0; i < blocksize; ++i)
+        for (int i = 0; i < framestoprocess; ++i)
         {
             float sample = osc_sin.step();
-            double gain = xenakios::decibelsToGain(volume_env.outputBlock[i]);
+            // double gain = xenakios::decibelsToGain(volume_env.outputBlock[i]);
+            double gain = volume_env.outputBlock[i];
+            gain = std::clamp(gain, 0.0, 1.0);
+            gain = gain * gain * gain;
             writebuf[framecount + i] = sample * gain;
         }
         framecount += blocksize;
