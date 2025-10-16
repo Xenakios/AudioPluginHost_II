a80370b0dd3e2183b5c060a03de0995ac32b2e3f
xenakios <xenakios@gmail.com>
2025-10-16 21:47:21 +0300

    added semisin osc and rewrote the processing logic

--- Source/PythonBindings/pybindings_ex4.cpp
+++ Source/PythonBindings/pybindings_ex4.cpp
@@ -12,7 +12,7 @@ namespace py = pybind11;
 inline py::array_t<float> generate_tone(int tone_type, xenakios::Envelope &pitch_env,
                                         xenakios::Envelope &volume_env, double sr, double duration)
 {
-    tone_type = std::clamp(tone_type, 0, 5);
+    tone_type = std::clamp(tone_type, 0, 6);
     pitch_env.sortPoints();
     volume_env.sortPoints();
     int chans = 1;
@@ -27,16 +27,17 @@ inline py::array_t<float> generate_tone(int tone_type, xenakios::Envelope &pitch
          sizeof(float)});
     py::array_t<float> output_audio{binfo};
     sst::basic_blocks::dsp::EBApproxSin<> osc_sin;
+    sst::basic_blocks::dsp::EBApproxSemiSin<> osc_semisin;
     sst::basic_blocks::dsp::EBTri<> osc_tri;
     sst::basic_blocks::dsp::EBSaw<> osc_saw;
     sst::basic_blocks::dsp::EBPulse<> osc_pulse;
     sst::basic_blocks::dsp::DPWSawOscillator<> osc_saw_dpw;
     sst::basic_blocks::dsp::DPWPulseOscillator<> osc_pulse_dpw;
     osc_sin.setSampleRate(sr);
+    osc_semisin.setSampleRate(sr);
     osc_tri.setSampleRate(sr);
     osc_saw.setSampleRate(sr);
     osc_pulse.setSampleRate(sr);
-
     const size_t blocksize = 8;
     int framecount = 0;
     float *writebuf = output_audio.mutable_data(0);
@@ -48,40 +49,44 @@ inline py::array_t<float> generate_tone(int tone_type, xenakios::Envelope &pitch
         double pitch = pitch_env.getValueAtPosition(tpos);
         pitch = std::clamp(pitch, 0.0, 136.0);
         double hz = 440.0 * std::pow(2.0, 1.0 / 12 * (pitch - 69.0));
-        if (tone_type == 0)
-            osc_sin.setFrequency(hz);
-        else if (tone_type == 1)
-            osc_tri.setFrequency(hz);
-        else if (tone_type == 2)
-            osc_saw.setFrequency(hz);
-        else if (tone_type == 3)
-            osc_pulse.setFrequency(hz);
-        else if (tone_type == 4)
-            osc_saw_dpw.setFrequency(hz, 1.0 / sr);
-        else if (tone_type == 5)
-            osc_pulse_dpw.setFrequency(hz, 1.0 / sr);
         volume_env.processBlock(tpos, sr, 0, blocksize);
-        for (int i = 0; i < framestoprocess; ++i)
-        {
-            float sample = 0.0f;
-            if (tone_type == 0)
-                sample = osc_sin.step();
-            else if (tone_type == 1)
-                sample = osc_tri.step();
-            else if (tone_type == 2)
-                sample = osc_saw.step();
-            else if (tone_type == 3)
-                sample = osc_pulse.step();
-            else if (tone_type == 4)
-                sample = osc_saw_dpw.step();
-            else if (tone_type == 5)
-                sample = osc_pulse_dpw.step();
-            // double gain = xenakios::decibelsToGain(volume_env.outputBlock[i]);
-            double gain = volume_env.outputBlock[i];
-            gain = std::clamp(gain, 0.0, 1.0);
-            gain = gain * gain * gain;
-            writebuf[framecount + i] = sample * gain * 0.8;
-        }
+        auto renderfunc = [&](auto &osc) {
+            if constexpr (std::is_same_v<decltype(osc),
+                                         sst::basic_blocks::dsp::DPWSawOscillator<> &> ||
+                          std::is_same_v<decltype(osc),
+                                         sst::basic_blocks::dsp::DPWPulseOscillator<> &>)
+            {
+                osc.setFrequency(hz, 1.0 / sr);
+            }
+            else
+            {
+                osc.setFrequency(hz);
+            }
+
+            for (int i = 0; i < framestoprocess; ++i)
+            {
+                float sample = osc.step();
+                double gain = volume_env.outputBlock[i];
+                gain = std::clamp(gain, 0.0, 1.0);
+                gain = gain * gain * gain;
+                writebuf[framecount + i] = sample * gain * 0.8;
+            }
+        };
+        if (tone_type == 0)
+            renderfunc(osc_sin);
+        else if (tone_type == 1)
+            renderfunc(osc_semisin);
+        else if (tone_type == 2)
+            renderfunc(osc_tri);
+        else if (tone_type == 3)
+            renderfunc(osc_saw);
+        else if (tone_type == 4)
+            renderfunc(osc_pulse);
+        else if (tone_type == 5)
+            renderfunc(osc_saw_dpw);
+        else if (tone_type == 6)
+            renderfunc(osc_pulse_dpw);
+
         framecount += blocksize;
     }
     return output_audio;
