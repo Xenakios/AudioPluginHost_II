1358c02831df7d85c2075ad7edbfd95975d42c98
xenakios <xenakios@gmail.com>
2026-03-18 18:34:29 +0200

    moved automation sequence to header

---
+++ Source/Common/automation_sequence.h
@@ -0,0 +1,82 @@
+#pragma once
+
+#include <vector>
+#include "containers/choc_NonAllocatingStableSort.h"
+#include "containers/choc_Span.h"
+
+struct AutomationEvent
+{
+    double timestamp; // seconds/beats
+    double value;     // parameter value
+    uint32_t id;      // parameter id (compatible with clap uint32_t id)
+    uint32_t flags;   // flags/extra data
+    uint16_t target;  // target synth voice/channel/effect submodule
+    bool operator<(const AutomationEvent &other) { return timestamp < other.timestamp; }
+};
+
+struct AutomationSequence
+{
+    std::vector<AutomationEvent> events;
+    AutomationSequence() { events.reserve(128); }
+    void add_event(double timestamp, uint32_t id, double value)
+    {
+        events.emplace_back(timestamp, value, id);
+    }
+    size_t size() const { return events.size(); }
+    void clear() { events.clear(); }
+    void sort_events() { choc::sorting::stable_sort(events.begin(), events.end()); }
+    struct Iterator
+    {
+        /// Creates an iterator positioned at the start of the sequence.
+        Iterator(const AutomationSequence &s, double sr) : owner(s), sampleRate(sr) {}
+        Iterator(const Iterator &) = default;
+        Iterator(Iterator &&) = default;
+
+        /// Seeks the iterator to the given time
+
+        void setTime(int64_t newTimeStamp)
+        {
+            auto eventData = owner.events.data();
+
+            while (nextIndex != 0 &&
+                   eventData[nextIndex - 1].timestamp * sampleRate >= newTimeStamp)
+                --nextIndex;
+
+            while (nextIndex < owner.events.size() &&
+                   eventData[nextIndex].timestamp * sampleRate < newTimeStamp)
+                ++nextIndex;
+
+            currentTime = newTimeStamp;
+        }
+
+        /// Returns the current iterator time
+        int64_t getTime() const noexcept { return currentTime; }
+
+        /// Returns a set of events which lie between the current time, up to (but not
+        /// including) the given duration. This function then increments the iterator to
+        /// set its current time to the end of this block.
+
+        choc::span<const AutomationEvent> readNextEvents(int duration)
+        {
+            auto start = nextIndex;
+            auto eventData = owner.events.data();
+            auto end = start;
+            auto total = owner.events.size();
+            auto endTime = currentTime + duration;
+            currentTime = endTime;
+
+            while (end < total && eventData[end].timestamp * sampleRate < endTime)
+                ++end;
+
+            nextIndex = end;
+
+            return {eventData + start, eventData + end};
+        }
+
+      private:
+        const AutomationSequence &owner;
+        int64_t currentTime = 0;
+        size_t nextIndex = 0;
+        double sampleRate = 0.0;
+    };
+};
