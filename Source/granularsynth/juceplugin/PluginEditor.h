c406447d2d55950ce0fb4976b82a64833c19d6b9
xenakios <xenakios@gmail.com>
2026-05-02 11:59:56 +0300

    moved dashboardcomponent to separate files

--- Source/granularsynth/juceplugin/PluginEditor.h
+++ Source/granularsynth/juceplugin/PluginEditor.h
@@ -2,6 +2,7 @@
 
 #include "PluginProcessor.h"
 #include "../../Experimental/xap_slider.h"
+#include "dashboardcomponent.h"
 
 class MyCustomLNF : public juce::LookAndFeel_V4
 {
@@ -749,114 +750,6 @@ class VolumeEnvelopeComponent : public juce::Component
     float priormorph = 0.0f;
     float priorauxwarp = 0.0f;
     bool auxenvmode = false;
-};
-
-class DashBoardComponent : public juce::Component
-{
-  public:
-    ToneGranulator *gr = nullptr;
-    std::vector<ToneGranulator::GrainVisualizerMessage> persisted_events;
-    struct ParamEvent
-    {
-        double timestamp = 0.0;
-        double value = 0.0;
-        double cpu_usage = 0.0;
-    };
-    std::vector<ParamEvent> paramValuesHistory;
-    juce::Path paramHistoryPath;
-    double timespantoshow = 8.0;
-    int throttlecounter = 0;
-    float visualfadecoefficient = 1.0;
-    bool showModulatorValues = false;
-
-    juce::ColourGradient pitchGradient;
-    std::unique_ptr<juce::VBlankAttachment> vblankAttachment;
-    std::function<double()> GetCPULoad;
-    DashBoardComponent(ToneGranulator *g) : gr(g)
-    {
-        paramHistoryPath.preallocateSpace(2048);
-        timespantoshow = gr->gvsettings.timespantoshow;
-        pitchGradient.clearColours();
-        pitchGradient.addColour(0.00, juce::Colours::red);
-        pitchGradient.addColour(0.25, juce::Colours::green);
-        pitchGradient.addColour(0.50, juce::Colours::yellow);
-        pitchGradient.addColour(0.75, juce::Colours::cyan);
-        pitchGradient.addColour(1.00, juce::Colours::white);
-        if (!is_debug())
-            visualfadecoefficient = 0.93;
-        else
-            visualfadecoefficient = std::pow(0.93, 4);
-        persisted_events.reserve(4096);
-        paramValuesHistory.reserve(1024);
-        vblankAttachment = std::make_unique<juce::VBlankAttachment>(this, [this]() {
-            updateGrainData();
-            if (is_debug())
-            {
-                if (throttlecounter % 1 == 0)
-                    repaint();
-                ++throttlecounter;
-            }
-            else
-            {
-                repaint();
-            }
-        });
-    }
-    void paint(juce::Graphics &g) override;
-    void paintAmbisonicFieldHammerProjection(juce::Graphics &g);
-    bool is_extended_size = false;
-    void mouseDown(const juce::MouseEvent &ev) override
-    {
-        if (!ev.mods.isRightButtonDown())
-            return;
-        juce::PopupMenu menu;
-        menu.addSectionHeader("Time span to show");
-        menu.addItem("1 second", [this]() { gr->gvsettings.timespantoshow = 1.0; });
-        menu.addItem("2 seconds", [this]() { gr->gvsettings.timespantoshow = 2.0; });
-        menu.addItem("4 seconds", [this]() { gr->gvsettings.timespantoshow = 4.0; });
-        menu.addItem("8 seconds", [this]() { gr->gvsettings.timespantoshow = 8.0; });
-        menu.addItem("16 seconds", [this]() { gr->gvsettings.timespantoshow = 16.0; });
-        menu.addSectionHeader("Options");
-        menu.addItem("Toggle size", [this]() {
-            is_extended_size = !is_extended_size;
-            getParentComponent()->resized();
-        });
-        menu.addItem("Show modulator values", true, showModulatorValues,
-                     [this]() { showModulatorValues = !showModulatorValues; });
-        juce::PopupMenu param_menu;
-        param_menu.addItem("-None-", [this]() { gr->modulatedParamToStore.store(0); });
-        for (auto &e : gr->parmetadatas)
-        {
-            if (e.flags & CLAP_PARAM_IS_MODULATABLE)
-            {
-                param_menu.addItem(e.groupName + "/" + e.name,
-                                   [this, id = e.id]() { gr->modulatedParamToStore.store(id); });
-            }
-        }
-        menu.addSubMenu("Parameter scope", param_menu);
-        menu.showMenuAsync(juce::PopupMenu::Options{});
-    }
-    void drawCPUGraph(juce::Graphics &g, double enginetime, double w, double xoffs);
-    void updateGrainData()
-    {
-        timespantoshow = gr->gvsettings.timespantoshow;
-        ToneGranulator::GrainVisualizerMessage msg;
-        while (gr->visualizer_fifo.pop(msg))
-        {
-            persisted_events.push_back(msg);
-        }
-        double enginetime = gr->playposframes / gr->m_sr;
-        std::erase_if(persisted_events, [this, enginetime](auto const &ev) {
-            return ev.timepos + ev.duration < enginetime - timespantoshow;
-        });
-        auto cpuload = 0.0;
-        if (GetCPULoad)
-            cpuload = GetCPULoad();
-        paramValuesHistory.emplace_back(enginetime, gr->modulatedParValueForGUI.load(), cpuload);
-        std::erase_if(paramValuesHistory, [this, enginetime](auto const &ev) {
-            return ev.timestamp < enginetime - timespantoshow;
-        });
-    }
 };
 
 class ParameterGroupComponent : public juce::GroupComponent
---
+++ Source/granularsynth/juceplugin/dashboardcomponent.cpp
@@ -0,0 +1,179 @@
+#include "dashboardcomponent.h"
+
+void DashBoardComponent::paintAmbisonicFieldHammerProjection(juce::Graphics &g)
+{
+    g.setColour(juce::Colours::white);
+    float ellipW = 400.0f;
+    float h = ellipW / 2;
+
+    float halfW = ellipW / 2.0f;
+    float halfH = h / 2.0;
+    float centerX = halfW;
+    float centerY = getHeight() / 2.0;
+    g.drawEllipse(0.0f, centerY - halfH, ellipW, h, 2.0f);
+
+    for (auto &e : persisted_events)
+    {
+        if (e.visualfade > 0.01)
+        {
+            auto drawgrain = [this, halfW, halfH, centerX, centerY, &g, &e](int chan) {
+                const float sqrt2 = 1.41421356f;
+                float radAzi = juce::degreesToRadians(-e.azimuth0degrees);
+                if (chan == 1)
+                    radAzi = juce::degreesToRadians(-e.azimuth1degrees);
+                float radElev = juce::degreesToRadians(e.elevationdegrees);
+                float cosElev = std::cos(radElev);
+                float cosHalfAzi = std::cos(radAzi * 0.5f);
+
+                // The term inside the sqrt can theoretically be slightly negative
+                // due to float precision errors at the very edge (180 deg).
+                float inner = 1.0f + cosElev * cosHalfAzi;
+                float d = std::sqrt(std::max(0.0f, inner));
+
+                // Safety: prevent division by zero at the singularity
+                if (d < 0.001f)
+                    d = 0.001f;
+
+                float x = (2.0f * sqrt2 * cosElev * std::sin(radAzi * 0.5f)) / d;
+                float y = (sqrt2 * std::sin(radElev)) / d;
+
+                // Mapping to your 2:1 Ellipse bounds
+                // This ensures that the dot stays perfectly within the visual ellipse
+                float scaleX = halfW / (2.0f * sqrt2);
+                float scaleY = halfH / sqrt2;
+
+                float pixelX = centerX + (x * scaleX);
+                float pixelY = centerY - (y * scaleY);
+                if (chan == 0)
+                    g.setColour(juce::Colours::lightgreen.withAlpha(e.visualfade));
+                else
+                    g.setColour(juce::Colours::lightcyan.withAlpha(e.visualfade));
+                g.fillEllipse(pixelX - 6.0f, pixelY - 6.0f, 12.0f, 12.0f);
+            };
+            if (std::abs(e.azimuth0degrees - e.azimuth1degrees) > 0.0f)
+            {
+                drawgrain(0);
+                drawgrain(1);
+            }
+            else
+            {
+                drawgrain(0);
+            }
+            e.visualfade = e.visualfade * visualfadecoefficient;
+        }
+    }
+}
+
+void DashBoardComponent::drawCPUGraph(juce::Graphics &g, double enginetime, double w, double xoffs)
+{
+    paramHistoryPath.clear();
+    for (size_t i = 0; i < paramValuesHistory.size(); ++i)
+    {
+        const auto &e = paramValuesHistory[i];
+        float xcor = w - ((enginetime - e.timestamp) / timespantoshow * w);
+        // xcor = std::clamp<float>(xcor + xoffs, xoffs, getWidth());
+        xcor = xcor + xoffs;
+        float ycor = juce::jmap<float>(e.cpu_usage, 0.0, 1.0, getHeight() - 5.0, 0.0);
+        if (i == 0)
+            paramHistoryPath.startNewSubPath(juce::Point<float>(xcor, ycor));
+        else
+            paramHistoryPath.lineTo(juce::Point<float>(xcor, ycor));
+    }
+    g.setColour(juce::Colours::beige.withAlpha(0.5f));
+    g.strokePath(paramHistoryPath, juce::PathStrokeType(3.0f));
+}
+
+void DashBoardComponent::paint(juce::Graphics &g)
+{
+    g.fillAll(juce::Colours::black);
+
+    if (showModulatorValues)
+    {
+        float barw = 16.0;
+        g.setColour(juce::Colours::green);
+        for (int i = 0; i < ToneGranulator::MIDICCEND - 32; ++i)
+        {
+            float xcor = i * barw;
+            float v = gr->modSourceValues[i];
+            if (v < 0.0)
+            {
+                float h = juce::jmap<float>(v, -1.0, 0.0, getHeight() / 2.0, 0.0);
+                g.fillRect(xcor, getHeight() / 2.0, barw - 1.0f, h);
+            }
+            else
+            {
+
+                float h = juce::jmap<float>(v, 0.0, 1.0, 0.0, getHeight() / 2.0);
+                g.fillRect(xcor, getHeight() / 2.0 - h, barw - 1.0f, h);
+            }
+        }
+    }
+    // g.setColour(juce::Colours::white);
+    float xoffs = 450.0f;
+    float w = getWidth() - xoffs;
+    double enginetime = gr->playposframes / gr->m_sr;
+    g.saveState();
+    g.reduceClipRegion(juce::Rectangle<int>(xoffs, 0, getWidth(), getHeight()));
+    for (auto &e : persisted_events)
+    {
+        float alpha = juce::jmap<float>(e.gain, 0.0f, 1.0f, 0.0f, 1.0f);
+        float normpitch = juce::jmap<float>(e.pitch, -48.0f, 64.0f, 0.0f, 1.0f);
+        g.setColour(pitchGradient.getColourAtPosition(normpitch).withBrightness(alpha));
+        float xcor = w - ((enginetime - e.timepos) / timespantoshow * w);
+        float ycor = juce::jmap<float>(e.pitch, -48.0, 64.0, getHeight() - 5.0, 0.0);
+        float gw = w / timespantoshow * e.duration;
+        // xcor = std::clamp<float>(xcor + xoffs, xoffs, getWidth());
+        xcor = xcor + xoffs;
+        g.fillEllipse(xcor, ycor, gw, 5.0);
+    }
+
+    paramHistoryPath.clear();
+    if (auto parid = gr->modulatedParamToStore.load())
+    {
+        auto &pmd = gr->idtoparmetadata[parid];
+        float parmin = pmd->minVal;
+        float parmax = pmd->maxVal;
+        if (parid == ToneGranulator::PAR_PITCH)
+        {
+            parmin = -48.0f;
+            parmax = 64.0f;
+        }
+        for (size_t i = 0; i < paramValuesHistory.size(); ++i)
+        {
+            const auto &e = paramValuesHistory[i];
+            float xcor = w - ((enginetime - e.timestamp) / timespantoshow * w);
+            // xcor = std::clamp<float>(xcor + xoffs, xoffs, getWidth());
+            xcor = xcor + xoffs;
+            float ycor = juce::jmap<float>(e.value, parmin, parmax, getHeight() - 5.0, 0.0);
+            if (i == 0)
+                paramHistoryPath.startNewSubPath(juce::Point<float>(xcor, ycor));
+            else
+                paramHistoryPath.lineTo(juce::Point<float>(xcor, ycor));
+        }
+        g.setColour(juce::Colours::beige.withAlpha(0.5f));
+        g.strokePath(paramHistoryPath, juce::PathStrokeType(3.0f));
+    }
+    drawCPUGraph(g, enginetime, w, xoffs);
+    g.restoreState();
+    paintAmbisonicFieldHammerProjection(g);
+    g.setColour(juce::Colours::yellow);
+    float h = juce::Decibels::gainToDecibels(gr->compensationgainforgui.load());
+    h = std::clamp(h, -40.0f, 0.0f);
+    h = juce::jmap<float>(h, -40.0, 0.0, 0.0, getHeight());
+    g.fillRect(juce::Rectangle<float>{0.0f, (float)getHeight() - h, 10.0f, h});
+    g.setColour(juce::Colours::white);
+    int mins = static_cast<int>(enginetime / 60.0);
+    int secs = static_cast<int>(std::fmod(enginetime, 60.0));
+    int ms = static_cast<int>(std::fmod(enginetime * 1000.0, 1000.0));
+
+    // 2. Format with leading zeros
+    juce::String timeText = juce::String::formatted("%02d:%02d.%03d", mins, secs, ms);
+    timeText += " " + juce::String(gr->missedgrains) + " missed grains";
+    // timeText += " " + juce::String(persisted_events.size()) + " events in history";
+    // timeText += " " + juce::String(gr->parmetadatas.size()) + " parameters";
+    timeText += " current snapshot : " + juce::String(gr->currentSnapShot);
+    timeText += " ambisonic order " + juce::String(gr->current_ambisonic_order);
+    timeText += " engine channels " + juce::String(gr->num_out_chans);
+    g.setFont(18.0f);
+    g.drawText(timeText, xoffs, 1.0f, getWidth() - xoffs, 25, juce::Justification::left);
+}
